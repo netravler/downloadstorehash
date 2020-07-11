506eca576454a936433bb1ab9fc962d425cd1868
netravler <pheintz@hotmail.com>
2020-07-11 15:02:23 -0500

    Add files via upload

---
+++ pullandchecksumwebpage.ps1
@@ -0,0 +1,14 @@
+﻿    # Powershell
+
+    clear
+
+    $url = "www.yahoo.com"
+
+	$response = (Invoke-WebRequest -Uri "http://$($url)" -UseBasicParsing).content
+
+    $response | Out-File -FilePath .\urltochecksum.txt -force
+
+    Get-FileHash .\urltochecksum.txt -Algorithm MD5 | Format-List | Out-File -filepath .\urlwithchecksum.txt
+
+    
+
